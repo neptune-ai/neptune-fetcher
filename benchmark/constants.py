32a487cca5ffe1993c75e91b5f56e9876f5fe2f3
AleksanderWWW <alwojnarowicz@gmail.com>
2024-06-07 15:16:22 +0200

    make config more generic

--- benchmark/constants.py
+++ benchmark/constants.py
@@ -1,10 +1,12 @@
 __all__ = ["FETCH_FLOAT_PROJECT", "FETCH_FLOAT_RUN_ID", "LIST_RUNS_PROJECT", "FETCH_DF_PROJECT"]
 
-WORKSPACE = "a.wojnarowicz/"
+from pathlib import Path
 
-FETCH_FLOAT_PROJECT = WORKSPACE + "fetch-floats"
+WORKSPACE = "<workspace-name>"
+
+FETCH_FLOAT_PROJECT = str(Path(WORKSPACE) / "fetch-floats")
 FETCH_FLOAT_RUN_ID = "TES-FLOATS-FETCH-1"
 
-LIST_RUNS_PROJECT = WORKSPACE + "list-runs"
+LIST_RUNS_PROJECT = str(Path(WORKSPACE) / "list-runs")
 
-FETCH_DF_PROJECT = WORKSPACE + "fetch-df"
+FETCH_DF_PROJECT = str(Path(WORKSPACE) / "fetch-df")
