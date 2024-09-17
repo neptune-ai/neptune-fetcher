c76fd4da8a720eb7c6e7df7fba61d115d6f5a43b
Krzysztof Godlewski <krzysztof.godlewski@neptune.ai>
2024-09-17 17:52:46 +0200

    Add e2e tests

---
+++ tests/e2e/test_dataframe_values.py
@@ -0,0 +1,101 @@
+import pytest
+
+
+@pytest.mark.parametrize("limit", [1, 2, 15, 100, 1000])
+def test__all_runs_limit(project, limit):
+    df = project.fetch_runs_df(limit=limit)
+    assert df.count().sum() == limit
+
+
+@pytest.mark.parametrize("limit", [1, 2, 15, 100, 1000])
+def test__all_experiments_limit(project, limit):
+    df = project.fetch_experiments_df(limit=limit)
+    assert df.count().sum() == limit
+
+
+@pytest.mark.parametrize("limit", [0, -1, -12345])
+def test__invalid_limit_raises_error(project, limit):
+    with pytest.raises(ValueError, match="limit.*greater than 0"):
+        project.fetch_runs_df(limit=limit)
+
+    with pytest.raises(ValueError, match="limit.*greater than 0"):
+        project.fetch_experiments_df(limit=limit)
+
+
+def test__nonexistent_column_is_returned_as_null(project, sys_columns):
+    df = project.fetch_runs_df(columns=["non_existent_column"] + sys_columns)
+    assert df["non_existent_column"].isnull().all()
+
+
+@pytest.mark.parametrize(
+    "regex, run_ids, expect_count",
+    [
+        ("config/.*unique-id-run-2", None, 100),
+        ("config/.*unique-id-run-1.*", None, 200),
+        (".*unique-id-run-2", None, 200),  # 100 unique config/* and metrics/*
+        ("metrics/.*|config/.*", ["id-run-5"], 40200),  # All columns in a project
+        ("non_existent_column", None, 0),
+    ],
+)
+def test__column_regex(project, regex, run_ids, expect_count):
+    df = project.fetch_runs_df(columns_regex=regex, custom_ids=run_ids)
+    assert len(df.columns) == expect_count
+
+
+def test__columns_regex_and_list_together(project, sys_columns):
+    """Request both specific columns and columns_regex. The returned set must strictly match."""
+
+    # Both sys attributes and metrics should be returned
+    df = project.fetch_runs_df(columns_regex="metrics/foo999.+", columns=sys_columns, custom_ids=["id-run-3"])
+    assert set(df.columns) == set(sys_columns + [f"metrics/foo999{x}" for x in range(10)])
+
+    # Passing a strict column name should return only that column even if
+    # we provide additional regex
+    df = project.fetch_runs_df(columns=["config/foo1"], columns_regex="sys/")
+    assert sum(not col.startswith("sys/") for col in df.columns) == 1, "There should be only 1 non-sys column returned"
+
+
+def test__columns_not_present_in_all_runs_are_null(project, sys_columns):
+    """
+    Request columns that exist only in some runs while explicitly requesting runs by ID.
+    The columns that don't exist in a run should be returned as nulls
+    """
+
+    # The requested column is unique for id-run-10
+    df = project.fetch_runs_df(
+        columns=["config/foo1-unique-id-run-10"],
+        custom_ids=["id-run-3", "id-run-10"],
+        sort_by="sys/custom_run_id",
+        ascending=True,
+    )
+
+    assert len(df) == 2
+    assert len(df.columns) == 1
+    assert df["config/foo1-unique-id-run-10"].isnull().sum() == 1  # Only id-run-10 has this column
+
+
+def test__columns_present_in_all_experiments(project, all_experiment_ids):
+    """Fetch columns that we know are part of all experiments"""
+
+    df = project.fetch_experiments_df(columns_regex="metrics/foo.*")
+    assert len(df) == len(all_experiment_ids)
+    assert df.count().sum() == len(all_experiment_ids) * 10000  # 10000 metrics/foo* per experiment
+
+
+def test__columns_must_match_runs(project, all_run_ids):
+    """Check if the returned columns match their expected rows (runs)"""
+
+    ids = all_run_ids[:3]
+
+    # Query just the unique columns. For each column, only a single run can have a non-null value.
+    df = project.fetch_runs_df(
+        columns_regex="config/.*9.*unique.*", custom_ids=ids, sort_by="sys/custom_run_id", ascending=True
+    )
+
+    assert len(df) == len(ids)
+    assert df.apply(lambda column: column.notna().sum() == 1).all(), "Each column must have only one non-null value"
+
+    # Make sure non-null columns actually match the run IDs
+    for col in df.columns:
+        row_with_value = df[col].notna().idxmax()
+        assert ids[row_with_value] in col, f"Column {col} must match the run ID {ids[row_with_value]}"
---
+++ tests/e2e/test_misc.py
@@ -0,0 +1,37 @@
+def test__list_runs(project, all_run_ids, all_experiment_ids, sys_columns_set):
+    result = list(project.list_runs())
+    ids = all_run_ids + all_experiment_ids
+    print(result)
+    assert all(set(run.keys()) == sys_columns_set for run in result), "All runs must have sys columns"
+
+    assert len(result) == len(ids)
+    assert set(run["sys/custom_run_id"] for run in result) == set(ids)
+
+
+def test__list_experiments(project, all_experiment_ids, sys_columns_set):
+    result = list(project.list_experiments())
+
+    assert all(set(run.keys()) == sys_columns_set for run in result), "All experiments must have sys columns"
+
+    assert len(result) == len(all_experiment_ids)
+    assert set(run["sys/custom_run_id"] for run in result) == set(all_experiment_ids)
+    assert all(len(run["sys/name"]) for run in result), "All experiments must have non-empty names"
+
+
+def test__fetch_runs(project, all_run_ids, all_experiment_ids, sys_columns):
+    result = project.fetch_runs()
+
+    ids = all_run_ids + all_experiment_ids
+
+    assert all(col in result.columns for col in sys_columns), "All sys columns must be present"
+    assert len(result) == len(ids)
+    assert set(result["sys/custom_run_id"]) == set(ids)
+
+
+def test__fetch_experiments(project, all_experiment_ids, sys_columns):
+    result = project.fetch_experiments()
+
+    assert all(col in result.columns for col in sys_columns), "All sys columns must be present"
+    assert len(result) == len(all_experiment_ids)
+    assert set(result["sys/custom_run_id"]) == set(all_experiment_ids)
+    assert all(len(name) for name in result["sys/name"]), "All experiments must have non-empty names"
