aa1d031b50c41a7e245d8da7b918aac9312d2e25
Krzysztof Godlewski <krzysztof.godlewski@neptune.ai>
2024-10-24 12:18:31 +0200

    Update `populate_projects.py` to match the latest `neptune_scale`

--- tests/populate_projects.py
+++ tests/populate_projects.py
@@ -56,7 +56,7 @@ def populate_run(run, run_id, tags=None):
         data |= {f"metrics/bar{x + 1}": value for x in range(MM_NUM_FIELD_KIND)}
         data |= {f"metrics/bar{x + 1}-unique-{run_id}": value for x in range(10)}
 
-        run.log_metrics(step, data=data)
+        run.log_metrics(data, step)
 
     # Last step will have a predetermined value
     step += 1
@@ -64,7 +64,7 @@ def populate_run(run, run_id, tags=None):
     data |= {f"metrics/bar{x + 1}-unique-{run_id}": x + 1 for x in range(10)}
     data |= {f"metrics/bar{x + 1}": x + 1 for x in range(MM_NUM_FIELD_KIND)}
 
-    run.log_metrics(step, data=data)
+    run.log_metrics(data, step)
 
 
 def populate_many_metrics(project):
