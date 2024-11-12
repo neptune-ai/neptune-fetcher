3cef58a4553f207ba01b3326fb9b016806a22f6e
Krzysztof Godlewski <krzysztof.godlewski@neptune.ai>
2024-11-12 14:30:13 +0100

    Handle exceptions in `backoff_retry`
    Also modify the signature for clarity: `max_tries` vs `max_retries`. Add
     unit tests.

--- src/neptune_fetcher/api/api_client.py
+++ src/neptune_fetcher/api/api_client.py
@@ -254,32 +254,60 @@ def create_auth_api_client(
 
 
 def backoff_retry(
-    func: Callable, max_retries: int = 5, backoff_factor: float = 0.5, max_backoff: float = 30.0, *args, **kwargs
+    func: Callable, *args, max_tries: int = 5, backoff_factor: float = 0.5, max_backoff: float = 30.0, **kwargs
 ) -> Response[Any]:
     """
-    Retries a function with exponential backoff.
+    Retries a function with exponential backoff. The function will be called at most `max_tries` times.
 
     :param func: The function to retry.
-    :param max_retries: Maximum number of retries.
+    :param max_tries: Maximum number of times `func` will be called, including retries.
     :param backoff_factor: Factor by which the backoff time increases.
     :param max_backoff: Maximum backoff time.
     :param args: Positional arguments to pass to the function.
     :param kwargs: Keyword arguments to pass to the function.
     :return: The result of the function call.
     """
-    retries = 0
-    while retries < max_retries:
-        response = func(*args, **kwargs)
-        code = response.status_code.value
-        if 0 <= code < 300:
-            return response
-        elif 500 <= code < 600 or code == 429:
-            retries += 1
-            backoff_time = min(backoff_factor * (2**retries), max_backoff)
-            time.sleep(backoff_time)
-        else:
-            raise NeptuneException(
-                f"Failed to get response after {retries} retries, "
-                f"status code: {response.status_code} "
-                f"response: {str(response.content)}"
-            )
+
+    if max_tries < 1:
+        raise ValueError("max_tries must be greater than or equal to 1")
+
+    tries = 0
+    last_exc = None
+    last_response = None
+
+    while True:
+        tries += 1
+        try:
+            response = func(*args, **kwargs)
+        except Exception as e:
+            response = None
+            last_exc = e
+
+        if response is not None:
+            last_response = response
+
+            code = response.status_code.value
+            if 0 <= code < 300:
+                return response
+
+            # Not a TooManyRequests or InternalServerError code
+            if not (code == 429 or 500 <= code < 600):
+                raise NeptuneException(f"Unexpected server response {response.status_code}: {str(response.content)}")
+
+        if tries == max_tries:
+            break
+
+        # A retryable error occurred, back off and try again
+        backoff_time = min(backoff_factor * (2**tries), max_backoff)
+        time.sleep(backoff_time)
+
+    # No more retries left
+    msg = []
+    if last_exc:
+        msg.append(f"Last exception: {str(last_exc)}")
+    if last_response:
+        msg.append(f"Last response: {last_response.status_code}: {str(last_response.content)}")
+    if not msg:
+        raise NeptuneException("Unknown error occurred when requesting data")
+
+    raise NeptuneException(f"Failed to get response after {tries} retries. " + "\n".join(msg))
