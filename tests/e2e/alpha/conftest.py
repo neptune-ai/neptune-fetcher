9661320ac95de5ca966833d84976d17f2816fbca
Paweł Ludwików <pawel.ludwikow@neptune.ai>
2025-01-23 10:45:26 +0100

    feat: use builder to get AuthenticatedClient

--- tests/e2e/alpha/conftest.py
+++ tests/e2e/alpha/conftest.py
@@ -1,4 +1,3 @@
-import os
 import uuid
 from datetime import (
     datetime,
@@ -6,27 +5,14 @@ from datetime import (
 )
 
 from neptune_api import AuthenticatedClient
-from neptune_api.credentials import Credentials
 from pytest import fixture
 
-from neptune_fetcher.api.api_client import (
-    create_auth_api_client,
-    get_config_and_token_urls,
-)
-
-API_TOKEN_ENV_NAME: str = "NEPTUNE_API_TOKEN"
+from neptune_fetcher.alpha.api_client import AuthenticatedClientBuilder
 
 
 @fixture(scope="module")
 def client() -> AuthenticatedClient:
-    api_token = os.getenv(API_TOKEN_ENV_NAME)
-    credentials = Credentials.from_api_key(api_key=api_token)
-    config, token_urls = get_config_and_token_urls(credentials=credentials, proxies=None)
-    client = create_auth_api_client(
-        credentials=credentials, config=config, token_refreshing_urls=token_urls, proxies=None
-    )
-
-    return client
+    return AuthenticatedClientBuilder.build()
 
 
 def unique_path(prefix):
