f07a3b7587aa1a56782c52c4cf86caf0b778fafe
Michał Sośnicki <michal.sosnicki@neptune.ai>
2025-02-20 07:05:12 +0100

    feat(alpha): add string_series to filters/types

--- src/neptune_fetcher/alpha/filters.py
+++ src/neptune_fetcher/alpha/filters.py
@@ -77,7 +77,9 @@ class AttributeFilter(BaseAttributeFilter):
     """
 
     name_eq: Union[str, list[str], None] = None
-    type_in: list[Literal["float", "int", "string", "bool", "datetime", "float_series", "string_set"]] = field(
+    type_in: list[
+        Literal["float", "int", "string", "bool", "datetime", "float_series", "string_set", "string_series"]
+    ] = field(
         default_factory=lambda: list(types.ALL_TYPES)  # type: ignore
     )
     name_matches_all: Union[str, list[str], None] = None
@@ -139,7 +141,9 @@ class Attribute:
 
     name: str
     aggregation: Optional[Literal["last", "min", "max", "average", "variance"]] = None
-    type: Optional[Literal["bool", "int", "float", "string", "datetime", "float_series", "string_set"]] = None
+    type: Optional[
+        Literal["bool", "int", "float", "string", "datetime", "float_series", "string_set", "string_series"]
+    ] = None
 
     def __post_init__(self) -> None:
         _validate_allowed_value(self.aggregation, types.ALL_AGGREGATIONS, "aggregation")
