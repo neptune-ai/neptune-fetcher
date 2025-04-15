37da049fd55a94f08f1735bbc3af8810aced587b
Michał Sośnicki <sosnicki.michal@gmail.com>
2025-04-15 16:08:59 +0200

    fix: functions in alpha allow positional args (#273)
    * fix: functions in alpha allow positional args
    
    * chore: remove alpha docstrings
    
    * tweak deprecation messages
    
    ---------
    
    Co-authored-by: Michał Sośnicki <michal.sosnicki@neptune.ai>
    Co-authored-by: Sabine <sabine.stahlberg@neptune.ai>

--- src/neptune_fetcher/alpha/__init__.py
+++ src/neptune_fetcher/alpha/__init__.py
@@ -26,13 +26,23 @@ __all__ = [
     "fetch_metrics",
 ]
 
+from typing import (
+    Literal,
+    Optional,
+    Tuple,
+    Union,
+)
+
+import pandas as _pandas
+
+from neptune_fetcher import Context
+from neptune_fetcher import fetch_experiments_table as _fetch_experiments_table
+from neptune_fetcher import fetch_metrics as _fetch_metrics
+from neptune_fetcher import filters as _filters
+from neptune_fetcher import get_context
+from neptune_fetcher import list_attributes as _list_attributes
+from neptune_fetcher import list_experiments as _list_experiments
 from neptune_fetcher import (
-    Context,
-    fetch_experiments_table,
-    fetch_metrics,
-    get_context,
-    list_attributes,
-    list_experiments,
     set_api_token,
     set_context,
     set_project,
@@ -40,6 +50,84 @@ from neptune_fetcher import (
 from neptune_fetcher.internal import warnings as _warnings
 
 _warnings.warn_deprecated(
-    "The `neptune_fetcher.alpha` package is deprecated and will be removed in future versions. "
-    "Please use the `neptune_fetcher` package instead.",
+    "The `neptune_fetcher.alpha` package is deprecated and will be removed in a future version. "
+    "Import directly from the `neptune_fetcher` package instead of `alpha`.",
 )
+
+
+def list_experiments(
+    experiments: Optional[Union[str, list[str], _filters.Filter]] = None,
+    context: Optional[Context] = None,
+) -> list[str]:
+    """
+    The alpha package is deprecated. Use `list_experiments` directly from the `neptune_fetcher` package instead.
+    """
+    return _list_experiments(
+        experiments=experiments,
+        context=context,
+    )
+
+
+def list_attributes(
+    experiments: Optional[Union[str, list[str], _filters.Filter]] = None,
+    attributes: Optional[Union[str, list[str], _filters.AttributeFilter]] = None,
+    context: Optional[Context] = None,
+) -> list[str]:
+    """
+    The alpha package is deprecated. Use `list_attributes` directly from the `neptune_fetcher` package instead.
+    """
+    return _list_attributes(
+        experiments=experiments,
+        attributes=attributes,
+        context=context,
+    )
+
+
+def fetch_metrics(
+    experiments: Union[str, list[str], _filters.Filter],
+    attributes: Union[str, list[str], _filters.AttributeFilter],
+    include_time: Optional[Literal["absolute"]] = None,
+    step_range: Tuple[Optional[float], Optional[float]] = (None, None),
+    lineage_to_the_root: bool = True,
+    tail_limit: Optional[int] = None,
+    type_suffix_in_column_names: bool = False,
+    include_point_previews: bool = False,
+    context: Optional[Context] = None,
+) -> _pandas.DataFrame:
+    """
+    The alpha package is deprecated. Use `fetch_metrics` directly from the `neptune_fetcher` package instead.
+    """
+    return _fetch_metrics(
+        experiments=experiments,
+        attributes=attributes,
+        include_time=include_time,
+        step_range=step_range,
+        lineage_to_the_root=lineage_to_the_root,
+        tail_limit=tail_limit,
+        type_suffix_in_column_names=type_suffix_in_column_names,
+        include_point_previews=include_point_previews,
+        context=context,
+    )
+
+
+def fetch_experiments_table(
+    experiments: Optional[Union[str, list[str], _filters.Filter]] = None,
+    attributes: Union[str, list[str], _filters.AttributeFilter] = "^sys/name$",
+    sort_by: Union[str, _filters.Attribute] = _filters.Attribute("sys/creation_time", type="datetime"),
+    sort_direction: Literal["asc", "desc"] = "desc",
+    limit: Optional[int] = None,
+    type_suffix_in_column_names: bool = False,
+    context: Optional[Context] = None,
+) -> _pandas.DataFrame:
+    """
+    The alpha package is deprecated. Use `fetch_experiments_table` directly from the `neptune_fetcher` package instead.
+    """
+    return _fetch_experiments_table(
+        experiments=experiments,
+        attributes=attributes,
+        sort_by=sort_by,
+        sort_direction=sort_direction,
+        limit=limit,
+        type_suffix_in_column_names=type_suffix_in_column_names,
+        context=context,
+    )
--- src/neptune_fetcher/alpha/runs.py
+++ src/neptune_fetcher/alpha/runs.py
@@ -21,9 +21,90 @@ __all__ = [
     "fetch_metrics",
 ]
 
-from neptune_fetcher.runs import (
-    fetch_metrics,
-    fetch_runs_table,
-    list_attributes,
-    list_runs,
+from typing import (
+    Literal,
+    Optional,
+    Tuple,
+    Union,
 )
+
+import pandas as _pandas
+
+from neptune_fetcher import filters as _filters
+from neptune_fetcher import runs as _runs
+from neptune_fetcher.internal import context as _context
+
+
+def list_runs(
+    runs: Optional[Union[str, list[str], _filters.Filter]] = None,
+    context: Optional[_context.Context] = None,
+) -> list[str]:
+    """
+    Deprecated. Use `list_runs` from the `neptune_fetcher.runs` package instead.
+    """
+    return _runs.list_runs(runs=runs, context=context)
+
+
+def list_attributes(
+    runs: Optional[Union[str, list[str], _filters.Filter]] = None,
+    attributes: Optional[Union[str, list[str], _filters.AttributeFilter]] = None,
+    context: Optional[_context.Context] = None,
+) -> list[str]:
+    """
+    Deprecated. Use `list_attributes` from the `neptune_fetcher.runs` package instead.
+    """
+    return _runs.list_attributes(
+        runs=runs,
+        attributes=attributes,
+        context=context,
+    )
+
+
+def fetch_metrics(
+    runs: Union[str, list[str], _filters.Filter],
+    attributes: Union[str, list[str], _filters.AttributeFilter],
+    include_time: Optional[Literal["absolute"]] = None,
+    step_range: Tuple[Optional[float], Optional[float]] = (None, None),
+    lineage_to_the_root: bool = True,
+    tail_limit: Optional[int] = None,
+    type_suffix_in_column_names: bool = False,
+    include_point_previews: bool = False,
+    context: Optional[_context.Context] = None,
+) -> _pandas.DataFrame:
+    """
+    Deprecated. Use `fetch_metrics` from the `neptune_fetcher.runs` package instead.
+    """
+    return _runs.fetch_metrics(
+        runs=runs,
+        attributes=attributes,
+        include_time=include_time,
+        step_range=step_range,
+        lineage_to_the_root=lineage_to_the_root,
+        tail_limit=tail_limit,
+        type_suffix_in_column_names=type_suffix_in_column_names,
+        include_point_previews=include_point_previews,
+        context=context,
+    )
+
+
+def fetch_runs_table(
+    runs: Optional[Union[str, list[str], _filters.Filter]] = None,
+    attributes: Union[str, list[str], _filters.AttributeFilter] = "^sys/name$",
+    sort_by: Union[str, _filters.Attribute] = _filters.Attribute("sys/creation_time", type="datetime"),
+    sort_direction: Literal["asc", "desc"] = "desc",
+    limit: Optional[int] = None,
+    type_suffix_in_column_names: bool = False,
+    context: Optional[_context.Context] = None,
+) -> _pandas.DataFrame:
+    """
+    Deprecated. Use `fetch_runs_table` from the `neptune_fetcher.runs` package instead.
+    """
+    return _runs.fetch_runs_table(
+        runs=runs,
+        attributes=attributes,
+        sort_by=sort_by,
+        sort_direction=sort_direction,
+        limit=limit,
+        type_suffix_in_column_names=type_suffix_in_column_names,
+        context=context,
+    )
