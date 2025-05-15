1b638b3b74463c9a185f6ce44448ba37bfcc6e46
Piotr Gabryjeluk <piotr.gabryjeluk@neptune.ai>
2025-05-15 19:34:54 +0200

    PY-105 Move alpha.exceptions out of alpha
    As exceptions will be most probably shared between versions of the API
    and are raised from version-independent internal code, I'm moving
    exceptions to top-level: `neptune_fetcher.alpha.exceptions` becomes
    `neptune_fetcher.exceptions`. Old name is aliased to the new name for
    compatibilty with existing scripts.

--- tests/e2e/alpha/internal/retrieval/xtest_attributes.py
+++ tests/e2e/alpha/internal/retrieval/xtest_attributes.py
@@ -9,7 +9,7 @@ from datetime import (
 
 import pytest
 
-from neptune_fetcher.alpha.exceptions import NeptuneProjectInaccessible
+from neptune_fetcher.exceptions import NeptuneProjectInaccessible
 from neptune_fetcher.alpha.filters import AttributeFilter
 from neptune_fetcher.internal.identifiers import (
     ProjectIdentifier,
