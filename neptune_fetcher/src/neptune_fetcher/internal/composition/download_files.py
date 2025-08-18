neptune-ai/neptune-fetcher

commit 1847c2ad7e0f186cb1d735e1ecd0af13adc445ae

PY-172 Improve the UX of download_files (#402)
* PY-172 Improve the UX of download_files

Internally:

1. Replace the single File.label field with experiment_name and run_id attributes, enforcing mutual exclusivity.
2. Encode project_identifier in the File object.
3. Shorten File's `repr`
4. Infer the column name for the output of `download_files` from the passed files rather than rely on which version of download_files is called (`nq.download_files` or `nq_runs.download_files`)

* Address code review notes

1. Don't allow mixing Files coming from experiment and run APIs
2. Fix bug calculating missing file paths
3. Adding a TODO for future update to `convert_table_to_dataframe`
4. Make create_files_dataframe accept `container_type` rather than `index_column_name`

* Update attribute_paths in file_object_* fixtures

neptune_fetcher/src/neptune_fetcher/internal/composition/download_files.py
@@ -156,5 +156,4 @@ def download_files(
         return output_format.create_files_dataframe(
             file_list,
             sys_id_label_mapping,
-            index_column_name="experiment" if container_type == search.ContainerType.EXPERIMENT else "run",
         )

src/neptune_query/internal/composition/download_files.py
@@ -22,7 +22,6 @@ import pandas as pd
 
 from ... import types
 from .. import client as _client
-from .. import identifiers
 from ..composition import (
     concurrency,
     validation,
@@ -42,7 +41,6 @@ def download_files(
     *,
     files: list[types.File],
     destination: pathlib.Path,
-    project_identifier: identifiers.ProjectIdentifier,
     container_type: ContainerType,
     context: Optional[Context] = None,
 ) -> pd.DataFrame:
@@ -54,11 +52,7 @@ def download_files(
 
         def generate_signed_files() -> Generator[tuple[types.File, _files.SignedFile], None, None]:
             for file_group in split_files(files):
-                signed_files = _files.fetch_signed_urls(
-                    client=client,
-                    project_identifier=project_identifier,
-                    file_paths=[file.path for file in file_group],
-                )
+                signed_files = _files.fetch_signed_urls(client=client, files=file_group)
                 yield from zip(file_group, signed_files)
 
         output = concurrency.generate_concurrently(
@@ -72,10 +66,7 @@ def download_files(
                         signed_file=file_tuple[1],
                         target_path=_files.create_target_path(
                             destination=destination,
-                            mime_type=file_tuple[0].mime_type,
-                            experiment_label=file_tuple[0].label,
-                            attribute_path=file_tuple[0].attribute_path,
-                            step=file_tuple[0].step,
+                            file=file_tuple[0],
                         ),
                     ),
                 )
@@ -91,6 +82,8 @@ def download_files(
         file_paths: dict[types.File, Optional[pathlib.Path]] = {}
         for file, path in results:
             file_paths[file] = path
+
         return create_files_dataframe(
-            file_paths, index_column_name="experiment" if container_type == ContainerType.EXPERIMENT else "run"
+            file_paths,
+            container_type=container_type,
         )

src/neptune_query/__init__.py
@@ -43,10 +43,11 @@ from neptune_query._internal import (
     get_default_project_identifier,
     resolve_attributes_filter,
     resolve_destination_path,
-    resolve_downloadable_files,
     resolve_experiments_filter,
+    resolve_files,
     resolve_sort_by,
 )
+from neptune_query.exceptions import NeptuneUserError
 from neptune_query.internal.composition import download_files as _download_files
 from neptune_query.internal.composition import fetch_metrics as _fetch_metrics
 from neptune_query.internal.composition import fetch_series as _fetch_series
@@ -370,7 +371,6 @@ def fetch_series(
 @use_query_metadata(api_function="download_files")
 def download_files(
     *,
-    project: Optional[str] = None,
     files: Union[types.File, Iterable[types.File], _pandas.Series, _pandas.DataFrame],
     destination: Optional[Union[str, pathlib.Path]] = None,
 ) -> _pandas.DataFrame:
@@ -381,8 +381,6 @@ def download_files(
     - For individually assigned files, use `fetch_experiments_table()`.
 
     Args:
-        project: Path of the Neptune project, as `WorkspaceName/ProjectName`.
-            If not provided, the NEPTUNE_PROJECT environment variable is used.
         files: Which files to download, specified using one of the following options.
             - File object
             - Iterable of File objects
@@ -410,12 +408,18 @@ def download_files(
         nq.download_files(files=interesting_files)
         ```
     """
-    project_identifier = get_default_project_identifier(project)
-    file_list = resolve_downloadable_files(files)
+    file_list = resolve_files(files)
     destination_path = resolve_destination_path(destination)
 
+    if not all(file.experiment_name for file in file_list):
+        raise NeptuneUserError(
+            "Some files passed to nq.download_files don't have associated experiment names. "
+            "This is likely because you passed files from the runs API. "
+            "Please use files from the experiments API instead by fetching them with "
+            "nq.fetch_series() or nq.fetch_experiments_table()."
+        )
+
     return _download_files.download_files(
-        project_identifier=project_identifier,
         files=file_list,
         destination=destination_path,
         container_type=_search.ContainerType.EXPERIMENT,

src/neptune_query/_internal.py
@@ -122,7 +122,7 @@ def get_default_project_identifier(project: Optional[str] = None) -> ProjectIden
     return ProjectIdentifier(project)
 
 
-def resolve_downloadable_files(
+def resolve_files(
     files: Union[File, Iterable[File], pd.Series, pd.DataFrame],
 ) -> list[File]:
     """

src/neptune_query/internal/composition/fetch_series.py
@@ -88,6 +88,7 @@ def fetch_series(
         if inference_result.is_run_domain_empty():
             return create_series_dataframe(
                 series_data={},
+                project_identifier=project_identifier,
                 sys_id_label_mapping={},
                 index_column_name="experiment" if container_type == ContainerType.EXPERIMENT else "run",
                 timestamp_column_name="absolute_time" if include_time == "absolute" else None,
@@ -155,6 +156,7 @@ def fetch_series(
 
         return create_series_dataframe(
             series_data,
+            project_identifier,
             sys_id_label_mapping,
             index_column_name="experiment" if container_type == ContainerType.EXPERIMENT else "run",
             timestamp_column_name="absolute_time" if include_time == "absolute" else None,

src/neptune_query/internal/composition/fetch_table.py
@@ -86,6 +86,7 @@ def fetch_table(
         if inference_result.is_run_domain_empty():
             return output_format.convert_table_to_dataframe(
                 table_data={},
+                project_identifier=project_identifier,
                 selected_aggregations={},
                 type_suffix_in_column_names=type_suffix_in_column_names,
                 index_column_name="experiment" if container_type == search.ContainerType.EXPERIMENT else "run",
@@ -104,6 +105,7 @@ def fetch_table(
         if sort_by_inference_result.is_run_domain_empty():
             return output_format.convert_table_to_dataframe(
                 table_data={},
+                project_identifier=project_identifier,
                 selected_aggregations={},
                 type_suffix_in_column_names=type_suffix_in_column_names,
                 index_column_name="experiment" if container_type == search.ContainerType.EXPERIMENT else "run",
@@ -176,6 +178,7 @@ def fetch_table(
     result_by_name = _map_keys_preserving_order(result_by_id, sys_id_label_mapping)
     dataframe = output_format.convert_table_to_dataframe(
         table_data=result_by_name,
+        project_identifier=project_identifier,
         selected_aggregations=selected_aggregations,
         type_suffix_in_column_names=type_suffix_in_column_names,
         index_column_name="experiment" if container_type == search.ContainerType.EXPERIMENT else "run",

src/neptune_query/internal/output_format.py
@@ -44,6 +44,7 @@ from .retrieval.metrics import (
     TimestampIndex,
     ValueIndex,
 )
+from .retrieval.search import ContainerType
 
 __all__ = (
     "convert_table_to_dataframe",
@@ -55,8 +56,11 @@ __all__ = (
 
 def convert_table_to_dataframe(
     table_data: dict[str, list[AttributeValue]],
+    project_identifier: str,
     selected_aggregations: dict[identifiers.AttributeDefinition, set[str]],
     type_suffix_in_column_names: bool,
+    # TODO: accept container_type as an argument instead of index_column_name
+    # see https://github.com/neptune-ai/neptune-fetcher/pull/402/files#r2260012199
     index_column_name: str = "experiment",
     flatten_aggregations: bool = False,
 ) -> pd.DataFrame:
@@ -93,8 +97,10 @@ def convert_table_to_dataframe(
                 for agg_name, agg_value in agg_subset_values.items():
                     if value.attribute_definition.type == "file_series" and agg_name == "last":
                         row[(column_name, "last")] = _create_output_file(
+                            project_identifier=project_identifier,
                             file=agg_value,
                             label=label,
+                            index_column_name=index_column_name,
                             attribute_path=value.attribute_definition.name,
                             step=getattr(aggregation_value, "last_step", None),
                         )
@@ -105,8 +111,10 @@ def convert_table_to_dataframe(
             elif value.attribute_definition.type == "file":
                 file_properties: File = value.value
                 row[(column_name, "")] = _create_output_file(
+                    project_identifier=project_identifier,
                     file=file_properties,
                     label=label,
+                    index_column_name=index_column_name,
                     attribute_path=value.attribute_definition.name,
                 )
             elif value.attribute_definition.type == "histogram":
@@ -305,6 +313,7 @@ def create_metrics_dataframe(
 
 def create_series_dataframe(
     series_data: dict[identifiers.RunAttributeDefinition, list[series.SeriesValue]],
+    project_identifier: str,
     sys_id_label_mapping: dict[identifiers.SysId, str],
     index_column_name: str,
     timestamp_column_name: Optional[str],
@@ -330,8 +339,10 @@ def create_series_dataframe(
                 series.SeriesValue(
                     step=point.step,
                     value=_create_output_file(
+                        project_identifier=project_identifier,
                         file=point.value,
                         label=label,
+                        index_column_name=index_column_name,
                         attribute_path=run_attribute_definition.attribute_definition.name,
                         step=point.step,
                     ),
@@ -444,8 +455,10 @@ def _sort_indices(df: pd.DataFrame) -> pd.DataFrame:
 
 def create_files_dataframe(
     file_data: dict[types.File, Optional[pathlib.Path]],
-    index_column_name: str = "experiment",
+    container_type: "ContainerType",
 ) -> pd.DataFrame:
+    index_column_name = "experiment" if container_type == ContainerType.EXPERIMENT else "run"
+
     if not file_data:
         return pd.DataFrame(
             index=pd.MultiIndex.from_tuples([], names=[index_column_name, "step"]),
@@ -455,7 +468,7 @@ def create_files_dataframe(
     rows: list[dict[str, Any]] = []
     for file, path in file_data.items():
         row = {
-            index_column_name: file.label,
+            index_column_name: file.container_identifier,
             "attribute": file.attribute_path,
             "step": file.step,
             "path": str(path) if path else None,
@@ -471,13 +484,19 @@ def create_files_dataframe(
 
 
 def _create_output_file(
+    project_identifier: str,
     file: File,
     label: str,
+    index_column_name: str,
     attribute_path: str,
     step: Optional[float] = None,
 ) -> types.File:
+    run_id = label if index_column_name == "run" else None
+    experiment_name = label if index_column_name == "experiment" else None
     return types.File(
-        label=label,
+        project_identifier=project_identifier,
+        experiment_name=experiment_name,
+        run_id=run_id,
         attribute_path=attribute_path,
         step=step,
         path=file.path,

src/neptune_query/internal/retrieval/files.py
@@ -38,6 +38,7 @@ from neptune_api.models import (
 from neptune_query.internal.query_metadata_context import with_neptune_client_metadata
 
 from ...exceptions import NeptuneFileDownloadError
+from ...types import File
 from .. import (
     env,
     identifiers,
@@ -56,24 +57,23 @@ class SignedFile:
 
 def fetch_signed_urls(
     client: AuthenticatedClient,
-    project_identifier: identifiers.ProjectIdentifier,
-    file_paths: list[str],
+    files: list[File] | list[SignedFile],
     permission: Literal["read", "write"] = "read",
 ) -> list[SignedFile]:
     body = CreateSignedUrlsRequest(
         files=[
-            FileToSign(project_identifier=project_identifier, path=file_path, permission=Permission(permission))
-            for file_path in file_paths
+            FileToSign(project_identifier=file.project_identifier, path=file.path, permission=Permission(permission))
+            for file in files
         ]
     )
     call_api = retry.handle_errors_default(with_neptune_client_metadata(signed_url_generic.sync_detailed))
     response = call_api(client=client, body=body)
 
     data: CreateSignedUrlsResponse = response.parsed
-    if len(data.files) != len(file_paths):
-        missing_paths = set(file_paths) - {file_.path for file_ in data.files}
+    if len(data.files) != len(files):
+        missing_paths = {f.path for f in files} - {f.path for f in data.files}
         raise ValueError(
-            f"Server returned {len(data.files)} / {len(file_paths)} signed urls. " f"Missing paths: {missing_paths}"
+            f"Server returned {len(data.files)} / {len(files)} signed urls. " f"Missing paths: {missing_paths}"
         )
     return [
         SignedFile(
@@ -104,12 +104,7 @@ def refresh_signed_file(
     Refreshes the signed file URL by fetching a new signed URL from the server.
     This is useful when the original signed URL has expired or is no longer valid.
     """
-    new_signed_files = fetch_signed_urls(
-        client=client,
-        project_identifier=signed_file.project_identifier,
-        file_paths=[signed_file.path],
-        permission=signed_file.permission,
-    )
+    new_signed_files = fetch_signed_urls(client=client, files=[signed_file], permission=signed_file.permission)
     return new_signed_files[0]
 
 
@@ -234,13 +229,11 @@ def download_file_complete(
         raise NeptuneFileDownloadError(details=f"Failed to download file after {max_tries} attempts.")
 
 
-def create_target_path(
-    destination: pathlib.Path, mime_type: str, experiment_label: str, attribute_path: str, step: Optional[float]
-) -> pathlib.Path:
-    relative_target_path = pathlib.Path(".") / experiment_label / attribute_path
-    if step is not None:
-        relative_target_path = relative_target_path / f"step_{step:f}"
-    extension = _guess_extension(mime_type)
+def create_target_path(destination: pathlib.Path, file: File) -> pathlib.Path:
+    relative_target_path = pathlib.Path(".") / file.container_identifier / file.attribute_path
+    if file.step is not None:
+        relative_target_path = relative_target_path / f"step_{file.step:f}"
+    extension = _guess_extension(file.mime_type)
 
     sanitized_parts = [_sanitize_path_part(part) for part in relative_target_path.parts]
     relative_target_path = pathlib.Path(*sanitized_parts)

commit ec7011d46d92ad1a9ed3e52bbd19c1b0ee9536be

Fix output regression in neptune_fetcher.runs.download_files
Introduced in #402 by accident

neptune_fetcher/src/neptune_fetcher/internal/composition/download_files.py
@@ -156,4 +156,5 @@ def download_files(
         return output_format.create_files_dataframe(
             file_list,
             sys_id_label_mapping,
+            index_column_name="experiment" if container_type == search.ContainerType.EXPERIMENT else "run",
         )