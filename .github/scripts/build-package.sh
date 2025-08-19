94636a0d21a838eebbce71884912278a3eec054b
Piotr Gabryjeluk <piotr.gabryjeluk@neptune.ai>
2025-08-19 14:29:12 +0200

    Update GitHub Actions setup

--- .github/scripts/build-package.sh
+++ .github/scripts/build-package.sh
@@ -1,55 +1,21 @@
 #!/bin/bash
 
 # Required params:
-#  - $1 - neptune_fetcher or neptune_query
-#  - $2 - version to build (git tag)
+#  - $1 - version to build (git tag)
 
 set -euo pipefail
 
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 <package_name> <git_ref>"
-  echo "Example: $0 neptune_fetcher neptune_fetcher/0.15.2-beta.2"
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <git_ref>"
+  echo "Example: $0 0.15.2-beta.2"
   exit 1
 fi
 
-PKG_NAME="$1"
-GIT_REF="$2"
-
-if [ "$PKG_NAME" != "neptune_fetcher" ] && [ "$PKG_NAME" != "neptune_query" ]; then
-    echo "Invalid package name: $PKG_NAME (expected neptune_fetcher or neptune_query)"
-    exit 1
-fi
-
-case "$GIT_REF" in
-  refs/tags/neptune_fetcher/*)
-    GIT_REF_PKG=neptune_fetcher
-  ;;
-  refs/tags/neptune_query/*)
-    GIT_REF_PKG=neptune_query
-  ;;
-  *)
-    echo "Invalid GIT_REF: $GIT_REF (expected refs/tags/neptune_fetcher/* or refs/tags/neptune_query/*)"
-    exit 1
-  ;;
-esac
-
-if [ "$GIT_REF_PKG" != "$PKG_NAME" ]; then
-  echo "Package name mismatch: $PKG_NAME (expected $GIT_REF_PKG). Not building"
-  exit 1
-fi
-
+GIT_REF="$1"
 VERSION=$(echo "$GIT_REF" | sed -e 's,.*/\(.*\),\1,')
 
 if [ -d dist ]; then
   rm -r dist
-fi
-
-if [ -d neptune_fetcher/dist ]; then
-  rm -r neptune_fetcher/dist
-fi
-
-if [ "$PKG_NAME" = "neptune_fetcher" ]; then
-  cd neptune_fetcher
 fi
 
 # TODO: Remove this workaround once https://github.com/orgs/community/discussions/4924 gets resolved
@@ -57,11 +23,6 @@ fi
 
 echo "VERSION=${VERSION}"
 POETRY_DYNAMIC_VERSIONING_BYPASS="$VERSION" poetry build
-
-if [ "$PKG_NAME" = "neptune_fetcher" ]; then
-  cd ..
-  mv neptune_fetcher/dist dist
-fi
 
 echo "Build completed. Here are the built files:"
 ls -lR dist/
